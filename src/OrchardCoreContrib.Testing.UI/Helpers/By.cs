a6bccf1d51a6e365ae4890fee64099a30bb5b705
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-04-28 06:20:29 +0300

    Add By helper

---
+++ src/OrchardCoreContrib.Testing.UI/Helpers/By.cs
@@ -0,0 +1,56 @@
+﻿using OrchardCoreContrib.Infrastructure;
+
+namespace OrchardCoreContrib.Testing.UI.Helpers;
+
+/// <summary>
+/// Represents a utility class for creating CSS selectors.
+/// </summary>
+public static class By
+{
+    /// <summary>
+    /// Generates a CSS selector for an element with the specified id.
+    /// </summary>
+    /// <param name="id">The element identifier.</param>
+    public static string Id(string id)
+    {
+        Guard.ArgumentNotNullOrEmpty(id, nameof(id));
+
+        return $"#{id}";
+    }
+
+    /// <summary>
+    /// Generates a CSS selector for an element with the specified class name.
+    /// </summary>
+    /// <param name="className">The element class name.</param>
+    public static string Class(string className)
+    {
+        Guard.ArgumentNotNullOrEmpty(className, nameof(className));
+
+        return $".{className}";
+    }
+
+    /// <summary>
+    /// Generates a CSS selector for an element with the specified name.
+    /// </summary>
+    /// <param name="name">The element name.</param>
+    public static string Name(string name)
+    {
+        Guard.ArgumentNotNullOrEmpty(name, nameof(name));
+
+        return name;
+    }
+
+    /// <summary>
+    /// Generates a CSS selector for an element with the specified attribute.
+    /// </summary>
+    /// <param name="name">The attribute name.</param>
+    /// <param name="value">The attribute value.</param>
+    public static string Attribute(string name, string value = null)
+    {
+        Guard.ArgumentNotNullOrEmpty(name, nameof(name));
+        
+        return value == null
+            ? $"[{name}]"
+            : $"[{name}='{value}']";
+    }
+}
---
+++ test/OrchardCoreContrib.Testing.UI.Tests/Helpers/ByTests.cs
@@ -0,0 +1,48 @@
+﻿namespace OrchardCoreContrib.Testing.UI.Helpers.Tests;
+
+public class ByTests
+{
+    [Fact]
+    public void ShouldGenerateCssSelectorByName()
+    {
+        // Act
+        var selector = By.Name("p");
+
+        // Assert
+        Assert.Equal("p", selector);
+    }
+
+    [Fact]
+    public void ShouldGenerateCssSelectorById()
+    {
+        // Act
+        var selector = By.Id("btnSend");
+
+        // Assert
+        Assert.Equal("#btnSend", selector);
+    }
+
+    [Fact]
+    public void ShouldGenerateCssSelectorByClass()
+    {
+        // Act
+        var selector = By.Class("red");
+
+        // Assert
+        Assert.Equal(".red", selector);
+    }
+
+    [InlineData("target", null, "[target]")]
+    [InlineData("name", "txtName", "[name='txtName']")]
+    [InlineData("type", "password", "[type='password']")]
+    [InlineData("type", "submit", "[type='submit']")]
+    [Theory]
+    public void ShouldGenerateCssSelectorByAttribute(string name, string value, string expectedSelector)
+    {
+        // Act
+        var selector = By.Attribute(name, value);
+
+        // Assert
+        Assert.Equal(expectedSelector, selector);
+    }
+}
