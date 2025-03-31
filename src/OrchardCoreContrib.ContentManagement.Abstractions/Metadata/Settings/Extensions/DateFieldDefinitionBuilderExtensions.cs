03221f0764a765bb090336aac339bd2048f4b850
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-03-31 11:04:43 +0300

    Fix DateField extension methods

--- src/OrchardCoreContrib.ContentManagement.Abstractions/Metadata/Settings/Extensions/DateFieldDefinitionBuilderExtensions.cs
+++ src/OrchardCoreContrib.ContentManagement.Abstractions/Metadata/Settings/Extensions/DateFieldDefinitionBuilderExtensions.cs
@@ -11,34 +11,13 @@ public static class DateFieldDefinitionBuilderExtensions
     /// Configures the Date Field to use the standard editor.
     /// </summary>
     /// <param name="builder"><see cref="ContentPartFieldDefinitionBuilder"/>.</param>
-    public static ContentPartFieldDefinitionBuilder WithHtmlFieldEditor(this ContentPartFieldDefinitionBuilder builder)
-        => builder.MergeSettings<ContentPartFieldSettings>(x => x.Editor = HtmlFieldEditor.Standard);
+    public static ContentPartFieldDefinitionBuilder WithDateFieldEditor(this ContentPartFieldDefinitionBuilder builder)
+        => builder.MergeSettings<ContentPartFieldSettings>(x => x.Editor = DateFieldEditor.Standard);
 
     /// <summary>
     /// Configures the Date Field to use the  What You See Is What You Get (WYSIWYG) editor.
     /// </summary>
     /// <param name="builder"><see cref="ContentPartFieldDefinitionBuilder"/>.</param>
-    public static ContentPartFieldDefinitionBuilder WithHtmlFieldWysiwygEditor(this ContentPartFieldDefinitionBuilder builder)
-        => builder.MergeSettings<ContentPartFieldSettings>(x => x.Editor = HtmlFieldEditor.Wysiwyg);
-
-    /// <summary>
-    /// Configures the Date Field to use the Trumbowyg editor.
-    /// </summary>
-    /// <param name="builder"><see cref="ContentPartFieldDefinitionBuilder"/>.</param>
-    public static ContentPartFieldDefinitionBuilder WithHtmlFieldTrumbowygEditor(this ContentPartFieldDefinitionBuilder builder)
-        => builder.MergeSettings<ContentPartFieldSettings>(x => x.Editor = HtmlFieldEditor.Trumbowyg);
-
-    /// <summary>
-    /// Configures the Date Field to use the Monaco editor.
-    /// </summary>
-    /// <param name="builder"><see cref="ContentPartFieldDefinitionBuilder"/>.</param>
-    public static ContentPartFieldDefinitionBuilder WithHtmlFieldMonacoEditor(this ContentPartFieldDefinitionBuilder builder)
-        => builder.MergeSettings<ContentPartFieldSettings>(x => x.Editor = HtmlFieldEditor.Monaco);
-
-    /// <summary>
-    /// Configures the Date Field to use the multi-line editor.
-    /// </summary>
-    /// <param name="builder"><see cref="ContentPartFieldDefinitionBuilder"/>.</param>
-    public static ContentPartFieldDefinitionBuilder WithHtmlFieldMultilineEditor(this ContentPartFieldDefinitionBuilder builder)
-        => builder.MergeSettings<ContentPartFieldSettings>(x => x.Editor = HtmlFieldEditor.Multiline);
+    public static ContentPartFieldDefinitionBuilder WithDateFieldLocalizedEditor(this ContentPartFieldDefinitionBuilder builder)
+        => builder.MergeSettings<ContentPartFieldSettings>(x => x.Editor = DateFieldEditor.Localized);
 }
