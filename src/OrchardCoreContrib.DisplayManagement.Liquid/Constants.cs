40e8f87ca7ee93e2c50b908e225bcccc982fa524
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-11-28 16:17:54 +0300

    Add OCC.DisplayManagement.Liquid

---
+++ src/OrchardCoreContrib.DisplayManagement.Liquid/Constants.cs
@@ -0,0 +1,18 @@
+﻿namespace OrchardCoreContrib.DisplayManagement.Liquid;
+
+/// <summary>
+/// Provides a container for constant values.
+/// </summary>
+public class Constants
+{
+	/// <summary>
+	/// Provides constant string values used as keys for fluid value operations.
+	/// </summary>
+	public static class FluidValueNames
+	{
+		/// <summary>
+		/// Represents the key name for the environment setting.
+		/// </summary>
+		public const string Environment = "Environment";
+    }
+}
