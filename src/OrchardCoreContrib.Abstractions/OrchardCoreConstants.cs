a5a6d86918d019f7c936d54488e6861b8d551d7a
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-03-28 04:17:18 +0300

    Add Stereotype list

---
+++ src/OrchardCoreContrib.Abstractions/OrchardCoreConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace OrchardCoreContrib;
+
+public static class OrchardCoreConstants
+{
+    public static class Stereotype
+    {
+        public const string CustomSettings = "CustomSettings";
+
+        public const string CustomUserSettings = "CustomUserSettings";
+
+        public const string DashboardWidget = "DashboardWidget";
+
+        public const string MenuItem = "MenuItem";
+
+        public const string Widget = "Widget";
+    }
+}
