69b7e6a15c574173be00eb353976001e7435b3fb
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-04-09 19:01:04 +0300

    Add GlobalSettingsAccessor

---
+++ src/OrchardCoreContrib.Infrastructure/Settings/GlobalSettingsAccessor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Environment.Shell;
+using OrchardCore.Settings;
+
+namespace OrchardCoreContrib.Settings;
+
+/// <summary>
+/// Provides access to global settings from the default tenant.
+/// </summary>
+/// <param name="shellHost">The shell host used to access tenant-specific services and settings.</param>
+public class GlobalSettingsAccessor(IShellHost shellHost) : IGlobalSettingsAccessor
+{
+    /// <inheritdoc/>
+    public async Task<TSettings> GetSettingsAsync<TSettings>() where TSettings : new()
+    {
+        if (shellHost.GetSettings(ShellSettings.DefaultShellName) is null)
+        {
+            throw new InvalidOperationException("Default tenant not found.");
+        }
+
+        var shellScope = await shellHost.GetScopeAsync(ShellSettings.DefaultShellName);
+
+        var siteService = shellScope.ServiceProvider.GetRequiredService<ISiteService>();
+
+        return await siteService.GetSettingsAsync<TSettings>();
+    }
+}
+
