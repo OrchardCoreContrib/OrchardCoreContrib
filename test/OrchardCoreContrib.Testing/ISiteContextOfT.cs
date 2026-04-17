51b68bc1b0633fda4f825dbf4da2ba95a5c2bde2
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-04-18 01:09:37 +0300

    Use OrchardCoreContrib.Testing

--- test/OrchardCoreContrib.Testing/ISiteContextOfT.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace OrchardCoreContrib.Testing;
-
-public interface ISiteContext<TSiteStartup> : ISiteContext where TSiteStartup : class
-{
-    static OrchardCoreWebApplicationFactory<TSiteStartup> Site { get; }
-}
