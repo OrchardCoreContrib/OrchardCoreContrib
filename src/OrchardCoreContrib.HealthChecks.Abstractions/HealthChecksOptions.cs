1f03b46642fb320c004c5ac1fbfc4a53171402aa
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-04-24 04:30:19 +0300

    Add LivenessPath & ReadinessPath to HealthChecksOptions

--- src/OrchardCoreContrib.HealthChecks.Abstractions/HealthChecksOptions.cs
+++ src/OrchardCoreContrib.HealthChecks.Abstractions/HealthChecksOptions.cs
@@ -11,6 +11,16 @@ public class HealthChecksOptions
     public string Url { get; set; } = "/health";
 
     /// <summary>
+    /// Gets or sets the HTTP endpoint path used to check application liveness. Default to "/health/live".
+    /// </summary>
+    public string LivenessPath { get; set; } = "/health/live";
+
+    /// <summary>
+    /// Gets or sets the HTTP endpoint path used to check application readiness. Default to "/health/ready".
+    /// </summary>
+    public string ReadinessPath { get; set; } = "/health/ready";
+
+    /// <summary>
     /// Whether to show the details for the checks dependency or not. Default to <c>false</c>.
     /// </summary>
     public bool ShowDetails { get; set; }
