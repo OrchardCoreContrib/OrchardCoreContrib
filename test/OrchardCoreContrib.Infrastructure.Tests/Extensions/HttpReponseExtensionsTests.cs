d339a0a7f023e2a9ede598947c2b18937b081e3a
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-05-18 15:12:54 +0300

    Add RedirectToAccessDeniedPage extension method

---
+++ test/OrchardCoreContrib.Infrastructure.Tests/Extensions/HttpReponseExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace OrchardCoreContrib.Infrastructure.Extensions.Tests;
+
+public class HttpReponseExtensionsTests
+{
+    [Fact]
+    public void RedirectToAccessDeniedPage_SetsRedirectLocation()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var cookieOptions = new CookieAuthenticationOptions
+        {
+            AccessDeniedPath = "/access-denied"
+        };
+
+        services.AddSingleton<IOptionsMonitor<CookieAuthenticationOptions>>(
+            new TestOptionsMonitor<CookieAuthenticationOptions>(cookieOptions));
+
+        var httpContext = new DefaultHttpContext
+        {
+            RequestServices = services.BuildServiceProvider()
+        };
+
+        var response = httpContext.Response;
+
+        // Act
+        response.RedirectToAccessDeniedPage();
+
+        // Assert
+        Assert.Equal(cookieOptions.AccessDeniedPath, response.Headers.Location.ToString());
+        Assert.Equal(StatusCodes.Status403Forbidden, response.StatusCode);
+    }
+
+    private sealed class TestOptionsMonitor<TOptions>(TOptions options) : IOptionsMonitor<TOptions>
+    {
+        public TOptions CurrentValue => options;
+
+        public TOptions Get(string name) => options;
+
+        public IDisposable OnChange(Action<TOptions, string> listener) => null;
+    }
+}
