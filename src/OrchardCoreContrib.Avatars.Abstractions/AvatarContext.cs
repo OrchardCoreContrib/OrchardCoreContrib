bddde74045e30fc5e5aa1340dfa15c8c066a3880
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-05-18 18:43:39 +0300

    Add Avatar abstraction & core APIs

---
+++ src/OrchardCoreContrib.Avatars.Abstractions/AvatarContext.cs
@@ -0,0 +1,22 @@
+﻿namespace OrchardCoreContrib.Avatars;
+
+/// <summary>
+/// Represents the context information for generating an avatar, including user details such as UserId, DisplayName, and Email.
+/// </summary>
+public class AvatarContext
+{
+    /// <summary>
+    /// Gets or sets the unique identifier of the user associated with the object.
+    /// </summary>
+    public string UserId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the display name associated with the object.
+    /// </summary>
+    public string DisplayName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the email address associated with the object.
+    /// </summary>
+    public string Email { get; set; }
+}
---
+++ src/OrchardCoreContrib.Avatars.Abstractions/IAvatarProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace OrchardCoreContrib.Avatars;
+
+/// <summary>
+/// Contract for providing avatar representations based on user information.
+/// </summary>
+public interface IAvatarProvider
+{
+    /// <summary>
+    /// Returns a string representing the avatar.
+    /// </summary>
+    /// <param name="context">The avatar context containing user information.</param>
+    /// <param name="size">Optional size of the avatar image.</param>
+    /// <returns>Could be a URL, a file path, or a Base64 inline image.</returns>
+    string GetAvatar(AvatarContext context, int size = 80);
+}
