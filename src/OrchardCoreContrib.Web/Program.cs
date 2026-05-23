68f70eba52dd31cdf6e9a0d38edac33d30c9f151
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-05-23 22:36:07 +0300

    Simplify Program.cs

--- src/OrchardCoreContrib.Web/Program.cs
+++ src/OrchardCoreContrib.Web/Program.cs
@@ -6,28 +6,18 @@ builder.Host.UseNLogHost();
 
 builder.Services
     .AddOrchardCms()
-// // Orchard Specific Pipeline
-// .ConfigureServices( services => {
-// })
-// .Configure( (app, routes, services) => {
-// })
-;
+    .AddSetupFeatures("OrchardCore.AutoSetup");
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
 app.UseOrchardCore();
 
-app.Run();
+await app.RunAsync();
 
 namespace OrchardCoreContrib.Web
 {
