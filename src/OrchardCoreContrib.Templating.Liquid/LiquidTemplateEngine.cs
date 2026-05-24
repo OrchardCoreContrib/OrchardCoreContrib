ec15f2adcb1583602ecb1d98accfdb237701db4a
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-05-24 03:05:51 +0300

    Add OCC.Templating.Liquid

---
+++ src/OrchardCoreContrib.Templating.Liquid/LiquidTemplateEngine.cs
@@ -0,0 +1,36 @@
+﻿using Fluid;
+using Fluid.Values;
+using System.Text.Encodings.Web;
+
+namespace OrchardCoreContrib.Templating.Liquid;
+
+/// <summary>
+/// Represents a template engine that uses the Liquid templating language.
+/// </summary>
+public class LiquidTemplateEngine : ITemplateEngine
+{
+    private readonly FluidParser _fluidParser = new FluidParser();
+
+    /// <inheritdoc/>
+    public string Name => "Liquid";
+
+    /// <inheritdoc/>
+    public async Task<string> RenderAsync(string template, TemplateContext context)
+    {
+        if (!_fluidParser.TryParse(template, out var parsedTemplate, out var errors))
+        {
+            throw new TemplateRenderException($"Liquid parsing failed: {string.Join(", ", errors)}");
+        }
+
+        var fluidTemplateContext = new Fluid.TemplateContext(context.Model);
+
+        try
+        {
+            return await parsedTemplate.RenderAsync(fluidTemplateContext);
+        }
+        catch (Exception ex)
+        {
+            throw new TemplateRenderException("Rendering liquid template failed.", ex);
+        }
+    }
+}
