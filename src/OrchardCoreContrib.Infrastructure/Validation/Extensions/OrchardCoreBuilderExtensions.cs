4815a8c21e67c0fbc04ceff622dc640202ff94be
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-05-30 23:27:41 +0300

    Don't break AddPhoneNumberValidator

--- src/OrchardCoreContrib.Infrastructure/Validation/Extensions/OrchardCoreBuilderExtensions.cs
+++ src/OrchardCoreContrib.Infrastructure/Validation/Extensions/OrchardCoreBuilderExtensions.cs
@@ -1,21 +1,17 @@
-﻿using OrchardCoreContrib.Validation;
+﻿using Microsoft.Extensions.DependencyInjection;
 
-namespace Microsoft.Extensions.DependencyInjection;
+namespace OrchardCoreContrib.Validation;
 
 /// <summary>
 /// Provides an extension methods for <see cref="OrchardCoreBuilder"/>.
 /// </summary>
-public static class ValidationOrchardCoreBuilderExtensions
+public static class OrchardCoreBuilderExtensions
 {
     /// <summary>
     /// Adds phone number validator service.
     /// </summary>
     /// <param name="builder">The <see cref="OrchardCoreBuilder"/>.</param>
+    [Obsolete("This method is deprecated and will be removed in the upcoming major release.", error: true)]
     public static OrchardCoreBuilder AddPhoneNumberValidator(this OrchardCoreBuilder builder)
-    {
-        builder.ConfigureServices(services =>
-            services.AddTransient<IPhoneNumberValidator, PhoneNumberValidator>());
-
-        return builder;
-    }
+        => ValidationOrchardCoreBuilderExtensions.AddPhoneNumberValidator(builder);
 }
