f487079039a5010d54df3ffff12cc14525c7f358
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-05-30 07:10:54 +0300

    More Guard methods

--- src/OrchardCoreContrib.Infrastructure.Abstractions/Guard.cs
+++ src/OrchardCoreContrib.Infrastructure.Abstractions/Guard.cs
@@ -1,10 +1,12 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace OrchardCoreContrib.Infrastructure;
 
 /// <summary>
 /// Represents an argument checker.
 /// </summary>
+[DebuggerStepThrough]
 public static partial class Guard
 {
     /// <summary>
@@ -12,11 +14,140 @@ public static partial class Guard
     /// </summary>
     /// <param name="value">The value to be tested.</param>
     /// <param name="name">The name of the tested value.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void ArgumentNotNull(object value, [CallerArgumentExpression(nameof(value))] string name = null)
     {
         if (value is null)
         {
             ArgumentNullException.ThrowIfNull(value, name);
+        }
+    }
+
+    /// <summary>
+    /// Asserts that the input value is of the specified type.
+    /// </summary>
+    /// <typeparam name="TType">The type to check against.</typeparam>
+    /// <param name="value">The value to be tested.</param>
+    /// <param name="name">The name of the tested value.</param>
+    /// <exception cref="ArgumentException">Thrown when the value is not of the specified type.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ArgumentIsOfType<TType>(object value, [CallerArgumentExpression(nameof(value))] string name = null)
+    {
+        if (value.GetType() != typeof(TType))
+        {
+            throw new ArgumentException($"The value must be of type {typeof(TType).Name}.", name);
+        }
+    }
+
+    /// <summary>
+    /// Asserts that the input value is not of the specified type.
+    /// </summary>
+    /// <typeparam name="TType">The type to check against.</typeparam>
+    /// <param name="value">The value to be tested.</param>
+    /// <param name="name">The name of the tested value.</param>
+    /// <exception cref="ArgumentException">Thrown when the value is of the specified type.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ArgumentIsNotOfType<TType>(object value, [CallerArgumentExpression(nameof(value))] string name = null)
+    {
+        if (value.GetType() == typeof(TType))
+        {
+            throw new ArgumentException($"The value must not be of type {typeof(TType).Name}.", name);
+        }
+    }
+
+    /// <summary>
+    /// Asserts that the input value is of the specified type.
+    /// </summary>
+    /// <param name="value">The value to be tested.</param>
+    /// <param name="type">The type to check against.</param>
+    /// <param name="name">The name of the tested value.</param>
+    /// <exception cref="ArgumentException">Thrown when the value is not of the specified type.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ArgumentIsOfType(object value, Type type, [CallerArgumentExpression(nameof(value))] string name = null)
+    {
+        if (value.GetType() != type)
+        {
+            throw new ArgumentException($"The value must be of type {type.Name}.", name);
+        }
+    }
+
+    /// <summary>
+    /// Asserts that the input value is not of the specified type.
+    /// </summary>
+    /// <param name="value">The value to be tested.</param>
+    /// <param name="type">The type to check against.</param>
+    /// <param name="name">The name of the tested value.</param>
+    /// <exception cref="ArgumentException">Thrown when the value is of the specified type.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ArgumentIsNotOfType(object value, Type type, [CallerArgumentExpression(nameof(value))] string name = null)
+    {
+        if (value.GetType() == type)
+        {
+            throw new ArgumentException($"The value must not be of type {type.Name}.", name);
+        }
+    }
+
+    /// <summary>
+    /// Asserts that the input value is assignable to the specified type.
+    /// </summary>
+    /// <typeparam name="TType">The type to check against.</typeparam>
+    /// <param name="value">The value to be tested.</param>
+    /// <param name="name">The name of the tested value.</param>
+    /// <exception cref="ArgumentException">Thrown when the value is not assignable to the specified type.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ArgumentIsAssignableToType<TType>(object value, [CallerArgumentExpression(nameof(value))] string name = null)
+    {
+        if (value is not TType)
+        {
+            throw new ArgumentException($"The value must be assignable to type {typeof(TType).Name}.", name);
+        }
+    }
+
+    /// <summary>
+    /// Asserts that the input value is not assignable to the specified type.
+    /// </summary>
+    /// <typeparam name="TType">The type to check against.</typeparam>
+    /// <param name="value">The value to be tested.</param>
+    /// <param name="name">The name of the tested value.</param>
+    /// <exception cref="ArgumentException">Thrown when the value is assignable to the specified type.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ArgumentIsNotAssignableToType<TType>(object value, [CallerArgumentExpression(nameof(value))] string name = null)
+    {
+        if (value is TType)
+        {
+            throw new ArgumentException($"The value must not be assignable to type {typeof(TType).Name}.", name);
+        }
+    }
+
+    /// <summary>
+    /// Asserts that the input value is assignable to the specified type.
+    /// </summary>
+    /// <param name="value">The value to be tested.</param>
+    /// <param name="type">The type to check against.</param>
+    /// <param name="name">The name of the tested value.</param>
+    /// <exception cref="ArgumentException">Thrown when the value is not assignable to the specified type.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ArgumentIsAssignableToType(object value, Type type, [CallerArgumentExpression(nameof(value))] string name = null)
+    {
+        if (!type.IsInstanceOfType(value))
+        {
+            throw new ArgumentException($"The value must be assignable to type {type.Name}.", name);
+        }
+    }
+
+    /// <summary>
+    /// Asserts that the input value is not assignable to the specified type.
+    /// </summary>
+    /// <param name="value">The value to be tested.</param>
+    /// <param name="type">The type to check against.</param>
+    /// <param name="name">The name of the tested value.</param>
+    /// <exception cref="ArgumentException">Thrown when the value is assignable to the specified type.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ArgumentIsNotAssignableToType(object value, Type type, [CallerArgumentExpression(nameof(value))] string name = null)
+    {
+        if (type.IsInstanceOfType(value))
+        {
+            throw new ArgumentException($"The value must not be assignable to type {type.Name}.", name);
         }
     }
 }
---
+++ test/OrchardCoreContrib.Infrastructure.Tests/Guard.StringTests.cs
@@ -0,0 +1,155 @@
+﻿using Xunit;
+
+namespace OrchardCoreContrib.Infrastructure.Tests;
+
+public partial class GuardTests
+{
+	[Fact]
+	public void ArgumentIsNullOrEmpty_NullValue_DoesNotThrow()
+	{
+		string value = null;
+
+		Guard.ArgumentIsNullOrEmpty(value);
+	}
+
+	[Fact]
+	public void ArgumentIsNullOrEmpty_EmptyValue_DoesNotThrow()
+	{
+		var value = string.Empty;
+
+		Guard.ArgumentIsNullOrEmpty(value);
+	}
+
+	[Fact]
+	public void ArgumentIsNullOrEmpty_NonEmptyValue_ThrowsArgumentNullOrEmptyException()
+	{
+		var value = "abc";
+
+		var exception = Assert.Throws<ArgumentNullOrEmptyException>(() => Guard.ArgumentIsNullOrEmpty(value));
+		Assert.Equal(nameof(value), exception.ParamName);
+	}
+
+	[Theory]
+	[InlineData(null)]
+	[InlineData("")]
+	public void ArgumentNotNullOrEmpty_NullOrEmptyValue_ThrowsArgumentNullException(string value)
+	{
+		var exception = Assert.Throws<ArgumentNullException>(() => Guard.ArgumentNotNullOrEmpty(value));
+		Assert.Equal(nameof(value), exception.ParamName);
+	}
+
+	[Fact]
+	public void ArgumentNotNullOrEmpty_NonEmptyValue_DoesNotThrow()
+	{
+		var value = "abc";
+
+		Guard.ArgumentNotNullOrEmpty(value);
+	}
+
+	[Theory]
+	[InlineData(null)]
+	[InlineData("")]
+	[InlineData("   ")]
+	public void ArgumentIsNullOrWhiteSpace_NullOrWhiteSpaceValue_DoesNotThrow(string value)
+	{
+		Guard.ArgumentIsNullOrWhiteSpace(value);
+	}
+
+	[Fact]
+	public void ArgumentIsNullOrWhiteSpace_NotWhiteSpaceValue_ThrowsArgumentNullException()
+	{
+		var value = "abc";
+
+		var exception = Assert.Throws<ArgumentNullException>(() => Guard.ArgumentIsNullOrWhiteSpace(value));
+		Assert.Equal(nameof(value), exception.ParamName);
+	}
+
+	[Theory]
+	[InlineData(null)]
+	[InlineData("")]
+	[InlineData("   ")]
+	public void ArgumentIsNotNullOrWhiteSpace_NullOrWhiteSpaceValue_ThrowsArgumentNullException(string value)
+	{
+		var exception = Assert.Throws<ArgumentNullException>(() => Guard.ArgumentIsNotNullOrWhiteSpace(value));
+		Assert.Equal(nameof(value), exception.ParamName);
+	}
+
+	[Fact]
+	public void ArgumentIsNotNullOrWhiteSpace_NotWhiteSpaceValue_DoesNotThrow()
+	{
+		var value = "abc";
+
+		Guard.ArgumentIsNotNullOrWhiteSpace(value);
+	}
+
+	[Fact]
+	public void ArgumentIsEmpty_EmptyValue_DoesNotThrow()
+	{
+		var value = string.Empty;
+
+		Guard.ArgumentIsEmpty(value);
+	}
+
+	[Theory]
+	[InlineData(null)]
+	[InlineData("abc")]
+	[InlineData(" ")]
+	public void ArgumentIsEmpty_NonEmptyValue_ThrowsArgumentException(string value)
+	{
+		var exception = Assert.Throws<ArgumentException>(() => Guard.ArgumentIsEmpty(value));
+		Assert.Equal(nameof(value), exception.ParamName);
+	}
+
+	[Fact]
+	public void ArgumentIsNotEmpty_EmptyValue_ThrowsArgumentException()
+	{
+		var value = string.Empty;
+
+		var exception = Assert.Throws<ArgumentException>(() => Guard.ArgumentIsNotEmpty(value));
+		Assert.Equal(nameof(value), exception.ParamName);
+	}
+
+	[Theory]
+	[InlineData("abc")]
+	[InlineData(" ")]
+	public void ArgumentIsNotEmpty_NonEmptyValue_DoesNotThrow(string value)
+	{
+		Guard.ArgumentIsNotEmpty(value);
+	}
+
+	[Theory]
+	[InlineData(null)]
+	[InlineData("")]
+	[InlineData("   ")]
+	public void ArgumentIsWhiteSpace_WhiteSpaceValue_DoesNotThrow(string value)
+	{
+		Guard.ArgumentIsWhiteSpace(value);
+	}
+
+	[Fact]
+	public void ArgumentIsWhiteSpace_NotWhiteSpaceValue_ThrowsArgumentException()
+	{
+		var value = "abc";
+
+		var exception = Assert.Throws<ArgumentException>(() => Guard.ArgumentIsWhiteSpace(value));
+		Assert.Equal(nameof(value), exception.ParamName);
+	}
+
+	[Theory]
+	[InlineData(null)]
+	[InlineData("")]
+	[InlineData("   ")]
+	public void ArgumentIsNotWhiteSpace_WhiteSpaceValue_ThrowsArgumentException(string value)
+	{
+		var exception = Assert.Throws<ArgumentException>(() => Guard.ArgumentIsNotWhiteSpace(value));
+		Assert.Equal(nameof(value), exception.ParamName);
+	}
+
+	[Fact]
+	public void ArgumentIsNotWhiteSpace_NotWhiteSpaceValue_DoesNotThrow()
+	{
+		var value = "abc";
+
+		Guard.ArgumentIsNotWhiteSpace(value);
+	}
+}
