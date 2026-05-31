df462344ff7efedd2c1cfa8d76d4167df810674a
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-05-31 03:04:28 +0300

    Fix navigation tests localizer setup

--- test/OrchardCoreContrib.Navigation.Tests/TestMenu1.cs
+++ test/OrchardCoreContrib.Navigation.Tests/TestMenu1.cs
@@ -3,15 +3,17 @@ using OrchardCore.Navigation;
 
 namespace OrchardCoreContrib.Navigation.Tests;
 
-public class TestMenu1(IStringLocalizer<TestMenu1> S) : NavigationProvider
+public class TestMenu1(IStringLocalizer<TestMenu1>? localizer) : NavigationProvider
 {
+    private readonly IStringLocalizer<TestMenu1>? _localizer = localizer;
+
     public override string Name => nameof(TestMenu1);
 
     public override Task BuildNavigationAsync(NavigationBuilder builder)
     {
         builder
-            .Add(S["Test"], test => test
-                .Add(S["Item 1"], item1 => item1
+            .Add(_localizer?["Test"] ?? new LocalizedString("Test", "Test"), test => test
+                .Add(_localizer?["Item 1"] ?? new LocalizedString("Item 1", "Item 1"), item1 => item1
                     .Url("#")
                     .Tenant("TestTenant")
                 )
